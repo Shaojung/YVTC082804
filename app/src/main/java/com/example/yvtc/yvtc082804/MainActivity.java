bf9a5ef6345d5ff97c049f3d06a947878a356870
Shaojung <shaojung@gmail.com>
2017-08-28 13:32:31 +0800

    fragment

--- app/src/main/java/com/example/yvtc/yvtc082804/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc082804/MainActivity.java
@@ -4,21 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = (Button) findViewById(R.id.button);
+        tv = (TextView) findViewById(R.id.textView3);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Test1", Toast.LENGTH_LONG).show();
+                tv.setText(new java.util.Date().toString());
             }
         });
     }
