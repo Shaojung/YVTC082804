60300b30be16bc1c453770e7846180fa57cf6969
Shaojung <shaojung@gmail.com>
2017-08-28 13:24:12 +0800

    click button

--- app/src/main/java/com/example/yvtc/yvtc082804/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc082804/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.yvtc.yvtc082804;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Test1", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
